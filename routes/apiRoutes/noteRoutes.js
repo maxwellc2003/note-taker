d5f814a4e624b3a4a276c956b434a984eec3c100
Max Colby <maxwe@MacBook-Pro.attlocal.net>
2022-10-21 18:22:37 -0700

    lost repo recovery

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const { createNewNote, validateNote } = require('../../lib/notes');
+const { notes } = require('../../db/notes');
+
+router.get('/notes', (req, res) => {
+    let results = notes;
+    res.json(results);
+  });
+
+router.post('/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+  
+    if (!validateNote(req.body)) {
+      res.status(400).send('The note is not properly formatted.');
+    } else {
+      const note = createNewNote(req.body, notes);
+      res.json(note);
+    }
+  });
+
+module.exports = router;
